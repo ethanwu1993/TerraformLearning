51524f768738206ac533d23022cc0bd5e2c5039e
Ethan Wu <ethanwu93@outlook.com>
2023-09-28 10:36:31 -0500

    multi

--- Multi-Region-deployment/main.tf
+++ Multi-Region-deployment/main.tf
@@ -1,162 +1,36 @@
 # Configure the AWS Provider
-provider "aws" {
-  alias = "east"
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias = "west"
-  region = "us-west-2"
-}
-
-#Retrieve the list of AZs in the current AWS region
-data "aws_availability_zones" "available" {}
-data "aws_region" "current" {}
-
-# Terraform Data Block - Lookup Ubuntu 20.04
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+module "my_s3_module_east" {
+  providers = {
+    aws = aws.east
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
+  source      = "./my-s3-module"
+  bucket_name = local.bucket_name_east
+  acl         = "private"
 }
 
-#Define the VPC 
-resource "aws_vpc" "vpc" {
-  provider = aws.east
-  cidr_block = var.vpc_cidr
 
-  tags = {
-    Name        = var.vpc_name
-    Environment = "demo_environment"
-    Terraform   = "true"
+module "my_s3_module_west" {
+  providers = {
+    aws = aws.west
+  }
+  source      = "./my-s3-module"
+  bucket_name = local.bucket_name_west
+  acl         = "private"
+}
+
+
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    actions = ["s3:GetObject", "s3:ListBucket"]
+    resources = ["arn:aws:s3:::${local.bucket_name_west}/*",
+      "arn:aws:s3:::${local.bucket_name_east}/*"
+    ]
   }
 }
 
-resource "aws_vpc" "vpc-west" {
-  provider = aws.west
-  cidr_block = "10.25.0.0/24"
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "s3-access-policy"
+  description = "Policy for S3 access"
 
-  tags = {
-    Name        = var.vpc_name
-    Environment = "disaster_recover_environment"
-    Terraform   = "true"
-  }
-}
-
-#Deploy the private subnets
-resource "aws_subnet" "private_subnets" {
-  for_each          = var.private_subnets
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(var.vpc_cidr, 8, each.value)
-  availability_zone = tolist(data.aws_availability_zones.available.names)[each.value]
-
-  tags = {
-    Name      = each.key
-    Terraform = "true"
-  }
-}
-
-#Deploy the public subnets
-resource "aws_subnet" "public_subnets" {
-  for_each                = var.public_subnets
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 8, each.value + 100)
-  availability_zone       = tolist(data.aws_availability_zones.available.names)[each.value]
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name      = each.key
-    Terraform = "true"
-  }
-}
-
-#Create route tables for public and private subnets
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.internet_gateway.id
-    #nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-  tags = {
-    Name      = "demo_public_rtb"
-    Terraform = "true"
-  }
-}
-
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    # gateway_id     = aws_internet_gateway.internet_gateway.id
-    nat_gateway_id = aws_nat_gateway.nat_gateway.id
-  }
-  tags = {
-    Name      = "demo_private_rtb"
-    Terraform = "true"
-  }
-}
-
-#Create route table associations
-resource "aws_route_table_association" "public" {
-  depends_on     = [aws_subnet.public_subnets]
-  route_table_id = aws_route_table.public_route_table.id
-  for_each       = aws_subnet.public_subnets
-  subnet_id      = each.value.id
-}
-
-resource "aws_route_table_association" "private" {
-  depends_on     = [aws_subnet.private_subnets]
-  route_table_id = aws_route_table.private_route_table.id
-  for_each       = aws_subnet.private_subnets
-  subnet_id      = each.value.id
-}
-
-#Create Internet Gateway
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "demo_igw"
-  }
-}
-
-#Create EIP for NAT Gateway
-resource "aws_eip" "nat_gateway_eip" {
-  vpc        = true
-  depends_on = [aws_internet_gateway.internet_gateway]
-  tags = {
-    Name = "demo_igw_eip"
-  }
-}
-
-#Create NAT Gateway
-resource "aws_nat_gateway" "nat_gateway" {
-  depends_on    = [aws_subnet.public_subnets]
-  allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id
-  tags = {
-    Name = "demo_nat_gateway"
-  }
-}
-
-# Terraform Resource Block - To Build EC2 instance in Public Subnet
-resource "aws_instance" "web_server" {                            # BLOCK
-  ami           = data.aws_ami.ubuntu.id                          # Argument with data expression
-  instance_type = "t2.micro"                                      # Argument
-  subnet_id     = aws_subnet.public_subnets["public_subnet_1"].id # Argument with value as expression
-  tags = {
-    Name = "Web EC2 Server"
-  }
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
